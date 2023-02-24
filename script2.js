ada2a5e87d0ab293a15132970a1e4faf75bf9374
Husseinturfah <@husseinturfah>
2023-02-25 00:36:32 +0200

    Added a way to display photos of dice through 'dice_number' which will be used later on to compare the two dice

--- script2.js
+++ script2.js
@@ -1,8 +1,14 @@
 window.onload = function(){
-
-
-
-
-
-  
+var dice = document.getElementsByClassName("dice");
+document.getElementById('refresh').addEventListener('mouseover',function(){
+  location.reload()
+}
+)
+const repeats_number = Math.floor(Math.random() * dice.length);
+console.log(repeats_number + 1)
+for (var i = 0; i < dice.length; i++){
+  dice[i].style.display = 'none'
+} 
+dice[repeats_number].style.display = 'block'
+console.log('hello working')
 }
