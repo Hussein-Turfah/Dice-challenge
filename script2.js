2ba0f1295933e0362dc5c728a9b51e74d0aa8a7b
Husseinturfah <@husseinturfah>
2023-02-25 01:00:22 +0200

    Used the same function of choosing randome 'dice1' in 'dice2' to choose random dice from the second list also

--- script2.js
+++ script2.js
@@ -14,4 +14,12 @@ for (var i = 0; i < dice1.length; i++){
 dice1[dice1_number].style.display = 'block'
 console.log('hello working')
 
+
+const dice2_number = Math.floor(Math.random() * dice2.length);
+console.log(dice2_number + 1)
+for (var i = 0; i < dice2.length; i++){
+  dice2[i].style.display = 'none'
+} 
+dice2[dice2_number].style.display = 'block'
+console.log('hello working')
 }
