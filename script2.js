d9b5fcae601a651bbb041df70ba051f16b38d0f8
Husseinturfah <@husseinturfah>
2023-02-25 13:23:07 +0200

    Cleaned the code and fixed some bugs

--- script2.js
+++ script2.js
@@ -1,36 +1,24 @@
 window.onload = function(){
 var dice1 = document.getElementsByClassName("dice1");
 var dice2 = document.getElementsByClassName("dice2");
-
 document.getElementById('refresh').addEventListener('click',function(){
-  location.reload()
-}
-)
-const dice1_number = Math.floor(Math.random() * dice1.length);
-console.log(dice1_number + 1)
-for (var i = 0; i < dice1.length; i++){
-  dice1[i].style.display = 'none'
-} 
-dice1[dice1_number].style.display = 'block'
-console.log('hello working')
+  location.reload()})
 
+  const dice1_number = Math.floor(Math.random() * dice1.length);
+for (var i = 0; i < dice1.length; i++){
+  dice1[i].style.display = 'none'} 
+dice1[dice1_number].style.display = 'block'
 
 const dice2_number = Math.floor(Math.random() * dice2.length);
-console.log(dice2_number + 1)
 for (var i = 0; i < dice2.length; i++){
-  dice2[i].style.display = 'none'
-} 
+  dice2[i].style.display = 'none'} 
 dice2[dice2_number].style.display = 'block'
-console.log('hello working')
 
 if(dice1_number<dice2_number){
-document.getElementById('result').innerHTML = "Player 2 Wins!😝"
-}
+document.getElementById('result').innerHTML = "Player 2 Wins!😝"}
 else if(dice1_number==dice2_number){
-  document.getElementById('result').innerHTML = "Draw!🥺"
-  }
-  else{
-    document.getElementById('result').innerHTML = "Player 1 Wins!😒"
-    }
+  document.getElementById('result').innerHTML = "Draw!🥺"}
+else{
+  document.getElementById('result').innerHTML = "Player 1 Wins!😍"}
       
 }
